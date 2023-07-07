4e42c7cd423d69c318796960d1f90cbf02ffd217
Szymon Gamża <gamza.szymon@gmail.com>
2023-07-07 02:24:38 +0200

    Added two pages and buttons

---
+++ AssetInfoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetScanner;
+
+public partial class AssetInfoPage : ContentPage
+{
+    public AssetInfoPage()
+    {
+        InitializeComponent();
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,18 +1,47 @@
-﻿namespace AssetScanner;
+﻿using Android.App;
+using Camera.MAUI.ZXingHelper;
+
+namespace AssetScanner;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
-	}
-
-
-    private void cameraView_CamerasLoaded(object sender, EventArgs e)
-    {
-
     }
+
+    private void StocktakingsButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new StocktakingsPage());
+    }
+
+    private void AssetInfoButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AssetInfoPage());
+    }
+
+
+
+    //private void cameraView_CamerasLoaded(object sender, EventArgs e)
+    //{
+    //    if (cameraView.Cameras.Count > 0)
+    //    {
+    //        cameraView.Camera = cameraView.Cameras.First();
+    //        MainThread.BeginInvokeOnMainThread(async () =>
+    //        {
+    //            await cameraView.StopCameraAsync();
+    //            await cameraView.StartCameraAsync();
+    //        });
+    //    }
+    //}
+    //
+    //private void CameraView_OnBarcodeDetected(object sender, BarcodeEventArgs args)
+    //{
+    //    MainThread.BeginInvokeOnMainThread(() =>
+    //    {
+    //        barcodeResult.Text = $"ODCZYTANY KOD QR: {args.Result[0].Text}";
+    //    });
+    //}
 }
 
