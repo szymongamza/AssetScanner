686bfd082220b91371495579a1b0ab1ef7e89ef5
Szymon Gamża <gamza.szymon@gmail.com>
2023-07-08 02:19:52 +0200

    Added page and qr reader

--- AssetInfoPage.xaml.cs
+++ AssetInfoPage.xaml.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Camera.MAUI.ZXingHelper;
 
 namespace AssetScanner;
 
 public partial class AssetInfoPage : ContentPage
 {
+    private bool isAssetPageOpen = false;
     public AssetInfoPage()
     {
         InitializeComponent();
+    }
+
+    private void CameraView_OnCamerasLoaded(object sender, EventArgs e)
+    {
+        if (CameraView.Cameras.Count > 0)
+        {
+            CameraView.Camera = CameraView.Cameras.First();
+            MainThread.BeginInvokeOnMainThread(async () =>
+            {
+                await CameraView.StopCameraAsync();
+                await CameraView.StartCameraAsync();
+            });
+        }
+    }
+
+    private void CameraView_OnBarcodeDetected(object sender, BarcodeEventArgs args)
+    {
+
+        if (!isAssetPageOpen)
+        {
+            isAssetPageOpen = true;
+
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                var assetPage = new AssetPage(args.Result[0].Text);
+                assetPage.Disappearing += (s, e) => { isAssetPageOpen = false; };
+                Navigation.PushAsync(assetPage);
+            });
+        }
     }
 }
---
+++ AssetPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetScanner;
+
+public partial class AssetPage : ContentPage
+{
+    public string QRCode { get; set;}
+    public AssetPage(string qrcode)
+    {
+        InitializeComponent();
+        QRCode = qrcode;
+        TempLabel.Text = qrcode;
+    }
+
+
+}
